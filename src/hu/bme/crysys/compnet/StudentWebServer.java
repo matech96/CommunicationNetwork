82f081c2a18286144b4629bb3c26fd1fb5d3eddf
matech <gangoly96@gmail.com>
2016-10-03 23:17:01 +0200

    Sending images is possible

--- src/hu/bme/crysys/compnet/StudentWebServer.java
+++ src/hu/bme/crysys/compnet/StudentWebServer.java
@@ -35,14 +35,14 @@ public class StudentWebServer extends Thread {
         try {
             String page = "";   //filename of the requested content
             String method = ""; //POST or HEAD or GET or something else, but that is not supported
-            int post_data_length = 0;   //if method is POST the length of the POST data should also set
+            int post_data_length = -1;   //if method is POST the length of the POST data should also set
 
             int responseCode = 200;
 
             //read
             while (true) {
                 String line = in.readLine();
-                if (line.startsWith("GET") || line.startsWith("POST") || line.startsWith("HEAD")) {
+                if (line.startsWith("GET ") || line.startsWith("POST ") || line.startsWith("HEAD ")) {
                     String[] line_split = line.split(" ");
                     page = line_split[1];  // get the page file from the header
                     page = page.split("\\?")[0];  // get rid of GET components
@@ -59,13 +59,6 @@ public class StudentWebServer extends Thread {
                 }
             }
 
-            // collect POST data to process in a further implementation
-            String post_data = "";
-            for (int i = 0; i < post_data_length; i++) {
-                int character = in.read();
-                post_data += (char)character;
-            }
-
             // handling main page
             if (page.equals("") || page.equals("/"))
                 page = "/index.html";
@@ -74,32 +67,55 @@ public class StudentWebServer extends Thread {
             String directory = "www";
             File file = new File(directory + page);
 
+            //Set responseCode
             if (method.equals("")) {
                 responseCode = 501; // not implemented status code
+            } else if (method.equals("POST") && post_data_length == -1){
+                responseCode = 400;  //bad request
             } else if (!file.exists()) {
                 responseCode = 404; //resource cannot be found
-                file = new File(directory + "/404.html"); //change the file
+                file = new File(directory + "/404.jpg"); //change the file
             }
 
+            // collect POST data to process in a further implementation
+            String post_data = "";
+            for (int i = 0; i < post_data_length; i++) {
+                int character = in.read();
+                post_data += (char)character;
+                //System.out.print((char)character);
+            }
+
+            System.out.println();
             System.out.println("Sending response to: " + page);
 
             // send header
             out.println("HTTP/1.0 " + responseCode);
             out.println("Server: Olcso Apache server");
 
-            //if method is GETor POST, send something back
-            if (method.equals("GET") && method.equals("POST")) {
+            //if method is GET or POST, send something back
+            if (method.equals("GET") || method.equals("POST")) {
                 //separate the header and the content
                 out.println("");
 
-                //Read the file and send it
-                BufferedReader fileReader = new BufferedReader(new FileReader(file));
-                while (true) {
-                    String line = fileReader.readLine();
-                    if (line == null)
-                        break;
 
-                    out.println(line);
+                FileInputStream fr = null;
+                OutputStream os = null;
+                try {
+                    fr = new FileInputStream(file);
+                    os = socket.getOutputStream();  //PrintWriter wont be god in we want to send binary f.e.: pictures
+                    int c;
+
+                    while ((c = fr.read()) != -1) {
+                        os.write(c);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    //close streams if they hav been created
+                    if (fr != null)
+                        fr.close();
+                    if (os != null)
+                        os.close();
                 }
             }
 
